a99c37c92e6b2941008c31c5bd46746513ca929f
Rukky90 <bennyfellow790@gmail.com>
2024-06-15 22:44:13 +0200

    initial commit of sphinx project

---
+++ sphinx_maths/maths/multiply.py
@@ -0,0 +1,12 @@
+def multiply_nums(num1, num2):
+    """This method will be used to multiply two numbers 
+        
+        :param float: The first number
+        :param float: The second number
+
+        :retruns: The result of the multiplication
+        :retype:float
+
+    """
+    answer = num1 * num2
+    return answer
