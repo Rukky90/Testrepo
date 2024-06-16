7bb27d79e71e4c8f9ab72f4d43d1d7ea483254fc
Rukky90 <bennyfellow790@gmail.com>
2024-06-16 16:30:17 +0200

    Committing changes before merge

---
+++ sphinx_maths/maths/divide.py
@@ -0,0 +1,12 @@
+def divide_nums(num1, num2):
+    """This method will be use to divide two numbers 
+
+      :param float num1: The dividend
+      :param float num2: The divisor
+
+      :return: The sum of the division 
+      :rtype: float
+    """
+ 
+    answer = num1 / num2
+    return answer
---
+++ sphinx_maths/maths/subtract.py
@@ -0,0 +1,11 @@
+def subtract_nums(num1, num2):
+    """This method will be use to subtract one number from another.
+
+       :param float: The number to be subtracted from.
+       :param float: The number to subtract.
+
+       :returns: The result of the subtrction
+       :rtype: float
+        """
+    answer = num2 - num1
+    return answer
